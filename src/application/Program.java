e2a583a86aa10d1450a7fe17c43248e674a401e0
Alex Pierini <alex_pierini@hotmail.com>
2020-03-14 16:59:49 -0400

    Première classe: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
 package application;
 
+import jeuDePlateau.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Bonjour le monde!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
 
 	}
 
---
+++ src/jeuDePlateau/Position.java
@@ -0,0 +1,37 @@
+package jeuDePlateau;
+
+public class Position {
+	private int ligne;
+	private int colonne;
+	
+	public Position() {
+	}
+	
+	public Position(int ligne, int colonne) {
+		this.ligne = ligne;
+		this.colonne = colonne;
+	}
+
+	public int getLigne() {
+		return ligne;
+	}
+
+	public void setLigne(int ligne) {
+		this.ligne = ligne;
+	}
+
+	public int getColonne() {
+		return colonne;
+	}
+
+	public void setColonne(int colonne) {
+		this.colonne = colonne;
+	}
+
+	@Override
+	public String toString() {
+		return ligne + ", " + colonne;
+	}
+	
+	
+}
