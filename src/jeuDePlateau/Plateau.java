80db1cfd3eda70377b137972d88a27179a1b9c42
Alex Pierini <alex_pierini@hotmail.com>
2020-03-15 11:12:31 -0400

    Plateau Exception et programmation défensive

--- src/jeuDePlateau/Plateau.java
+++ src/jeuDePlateau/Plateau.java
@@ -6,6 +6,10 @@ public class Plateau {
 	private Piece[][] pieces;
 	
 	public Plateau(int lignes, int colonnes) {
+		if(lignes < 1 || colonnes < 1) {
+			throw new PlateauException("Erreur lors de la création du plateau: il doit y avoir au moins 1 ligne et 1 colonne.");
+		}
+		
 		this.lignes = lignes;
 		this.colonnes = colonnes;
 		this.pieces = new Piece[lignes][colonnes];
@@ -15,28 +19,47 @@ public class Plateau {
 		return lignes;
 	}
 
-	public void setLignes(int lignes) {
-		this.lignes = lignes;
-	}
-
 	public int getColonnes() {
 		return colonnes;
 	}
 
-	public void setColonnes(int colonnes) {
-		this.colonnes = colonnes;
-	}
-	
 	public Piece piece(int ligne, int colonne) {
+		if (!laPositionExiste(ligne, colonne)) {
+			throw new PlateauException("Position inexistante sur le plateau.");
+		}
+		
 		return pieces[ligne][colonne];
 	}
 	
 	public Piece piece(Position position) {
+		if (!laPositionExiste(position)) {
+			throw new PlateauException("Position inexistante sur le plateau.");
+		}
+		
 		return pieces[position.getLigne()][position.getColonne()];
 	}
 	
 	public void placerPiece(Piece piece, Position position) {
+		if (ilYAUnPiece(position)) {
+			throw new PlateauException("Il y a déjà une pièce sur la position " + position);
+		}
 		this.pieces[position.getLigne()][position.getColonne()] = piece;
 		piece.position = position;
+	}
+	
+	public boolean laPositionExiste(int ligne, int colonne) {
+		return ligne >= 0 && ligne < this.lignes && colonne >= 0 && colonne < this.colonnes;
+	}
+	
+	public boolean laPositionExiste(Position position) {
+		return laPositionExiste(position.getLigne(), position.getColonne());
+	}
+	
+	public boolean ilYAUnPiece(Position position) {
+		if (!laPositionExiste(position)) {
+			throw new PlateauException("Position inexistante sur le plateau.");
+		}
+		
+		return piece(position) != null;
 	}
 }
---
+++ src/jeuDePlateau/PlateauException.java
@@ -0,0 +1,9 @@
+package jeuDePlateau;
+
+public class PlateauException extends RuntimeException{
+	private static final long serialVersionUID = 1L;
+	
+	public PlateauException(String msg) {
+		super(msg);
+	}
+}
