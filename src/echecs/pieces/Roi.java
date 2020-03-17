43f214a36bade7113430c04227dd8d4b76a08e1e
Alex Pierini <alex_pierini@hotmail.com>
2020-03-16 23:32:42 -0400

    Mise en œuvre des mouvements possibles du roi.

--- src/echecs/pieces/Roi.java
+++ src/echecs/pieces/Roi.java
@@ -3,6 +3,7 @@ package echecs.pieces;
 import echecs.Couleur;
 import echecs.PieceEchecs;
 import jeuDePlateau.Plateau;
+import jeuDePlateau.Position;
 
 public class Roi extends PieceEchecs {
 	public Roi(Plateau plateau, Couleur couleur) {
@@ -13,10 +14,66 @@ public class Roi extends PieceEchecs {
 	public String toString() {
 		return "R";
 	}
+	
+	private boolean peutSeDeplacer(Position position) {
+		PieceEchecs p = (PieceEchecs)getPlateau().piece(position);
+		return p == null || p.getCouleur() != getCouleur();
+	}
 
 	@Override
 	public boolean[][] mouvementsPossibles() {
 		boolean[][] matrice = new boolean[getPlateau().getLignes()][getPlateau().getColonnes()];
+		
+		Position p = new Position(0, 0);
+		
+		// dessus
+		p.setValeurs(position.getLigne() - 1 , position.getColonne());
+		if (getPlateau().laPositionExiste(p) && peutSeDeplacer(p)) {
+			matrice[p.getLigne()][p.getColonne()] = true;
+		}
+		
+		// dessous
+		p.setValeurs(position.getLigne() + 1 , position.getColonne());
+		if (getPlateau().laPositionExiste(p) && peutSeDeplacer(p)) {
+			matrice[p.getLigne()][p.getColonne()] = true;
+		}
+		
+		// gauche
+		p.setValeurs(position.getLigne(), position.getColonne() - 1);
+		if (getPlateau().laPositionExiste(p) && peutSeDeplacer(p)) {
+			matrice[p.getLigne()][p.getColonne()] = true;
+		}
+		
+		// droite
+		p.setValeurs(position.getLigne(), position.getColonne() + 1);
+		if (getPlateau().laPositionExiste(p) && peutSeDeplacer(p)) {
+			matrice[p.getLigne()][p.getColonne()] = true;
+		}
+		
+		// nord-ouest
+		p.setValeurs(position.getLigne() - 1, position.getColonne() - 1);
+		if (getPlateau().laPositionExiste(p) && peutSeDeplacer(p)) {
+			matrice[p.getLigne()][p.getColonne()] = true;
+		}
+		
+		// nord-est
+		p.setValeurs(position.getLigne() - 1, position.getColonne() + 1);
+		if (getPlateau().laPositionExiste(p) && peutSeDeplacer(p)) {
+			matrice[p.getLigne()][p.getColonne()] = true;
+		}
+		
+		// sud-ouest
+		p.setValeurs(position.getLigne() + 1, position.getColonne() - 1);
+		if (getPlateau().laPositionExiste(p) && peutSeDeplacer(p)) {
+			matrice[p.getLigne()][p.getColonne()] = true;
+		}
+		
+		// sud-est
+		p.setValeurs(position.getLigne() + 1, position.getColonne() + 1);
+		if (getPlateau().laPositionExiste(p) && peutSeDeplacer(p)) {
+			matrice[p.getLigne()][p.getColonne()] = true;
+		}
+
 		return matrice;
 	}
 }
